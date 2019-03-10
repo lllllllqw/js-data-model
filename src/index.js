44f44ca335ce6348e8176063ca221ca1f55ff554
lllllllqw <465633856@qq.com>
2019-03-10 18:01:25 +0800

    feat: support model`s value of key be a function, use with custom parser

--- src/index.js
+++ src/index.js
@@ -11,60 +11,77 @@ import * as utils from './utils'
 
 /** @type {Map<any, (val: any, option: Options, helperData: {}) => any>} */
 const parser = new Map([
-  [Array, (val, option, helperData) => {
-    const isArray = Object.prototype.toString.call(val) === '[object Array]'
+  [
+    Array,
+    (val, option, helperData) => {
+      const isArray = Object.prototype.toString.call(val) === '[object Array]'
 
-    // 没有值 | 值类型不是 Array, 返回默认的空数组
-    if (utils.isNoVal(val) || !isArray) {
-      return utils.getDefaultValue(option.default) || []
-    }
-    
-    // 定义 model 且 类型值是 Array, 按 model 返回
-    if(option.subModel && isArray) {
-      return val.map((item) => parse(item, option.subModel, helperData))
-    }
-
-    if(isArray) {
-    // 返回浅拷贝的数组
-      return [...val]
-    }
-  }],
-  [Object, (val, option, helperData) => {
-    // 如果定义了 model, 按 model 返回
-    if(option.subModel) {
-      return parse(val, option.subModel, helperData)
-    }
-    
-    // 没有值且默认值不为 undefined, 返回默认值
-    if (utils.isNoVal(val)) {
-      const defaultValue = utils.getDefaultValue(option.default)
-      if(defaultValue !== undefined) {
-        return defaultValue
+      // 没有值 | 值类型不是 Array, 返回默认的空数组
+      if (utils.isNoVal(val) || !isArray) {
+        return utils.getDefaultValue(option.default) || []
       }
-    }
 
-    // 有值且不为 Object 类型, 返回 {}
-    if (Object.prototype.toString.call(val) !== '[object Object]') {
-      return {}
-    }
-    
-    // 返回浅拷贝的原值
-    return {...val}
-  }],
+      // 定义 model 且 类型值是 Array, 按 model 返回
+      if (option.subModel && isArray) {
+        return val.map(item => parse(item, option.subModel, helperData))
+      }
+
+      if (isArray) {
+        // 返回浅拷贝的数组
+        return [...val]
+      }
+    },
+  ],
+  [
+    Object,
+    (val, option, helperData) => {
+      // 如果定义了 model, 按 model 返回
+      if (option.subModel) {
+        return parse(val, option.subModel, helperData)
+      }
+
+      // 没有值且默认值不为 undefined, 返回默认值
+      if (utils.isNoVal(val)) {
+        const defaultValue = utils.getDefaultValue(option.default)
+        if (defaultValue !== undefined) {
+          return defaultValue
+        }
+      }
+
+      // 有值且不为 Object 类型, 返回 {}
+      if (Object.prototype.toString.call(val) !== '[object Object]') {
+        return {}
+      }
+
+      // 返回浅拷贝的原值
+      return { ...val }
+    },
+  ],
   // Object & Array => JSON.stringify(val) any -> any.toString()
-  [String, (val, option) => {
-    if(utils.isNoVal(val)) {
-      return utils.getDefaultValue(option.default)
-    }
+  [
+    String,
+    (val, option) => {
+      if (utils.isNoVal(val)) {
+        return utils.getDefaultValue(option.default)
+      }
 
-    if(['[object Object]', '[object Array]'].includes(Object.prototype.toString.call(val))) {
-      return JSON.stringify(val)
-    }
+      if (
+        ['[object Object]', '[object Array]'].includes(
+          Object.prototype.toString.call(val)
+        )
+      ) {
+        return JSON.stringify(val)
+      }
 
-    return val.toString()
-  }],
+      return val.toString()
+    },
+  ],
   // [undefined, null, false] -> 0, true -> 1, String -> [number, NaN]
-  [Number, (val, option) => utils.isNoVal(val) ? utils.getDefaultValue(option.default) : +val],
+  [
+    Number,
+    (val, option) =>
+      utils.isNoVal(val) ? utils.getDefaultValue(option.default) : +val,
+  ],
 ])
 
 /**
@@ -81,9 +98,14 @@ const parse = (data, model, helperData = {}) => {
   // 遍历 model, 对每一个 model 进行解析
   for (const [key, option] of Object.entries(model)) {
     // 优先从 fromKey 字段获取原值
-    const val = option.fromKey ? utils.get(data, option.fromKey) : utils.get(data, key)
+    const val = option.fromKey
+      ? utils.get(data, option.fromKey)
+      : utils.get(data, key)
 
     // 如果存在 parser, 优先使用 parser 解析
+    if (typeof option === 'function') {
+      result[key] = option(val, { data, model, option, helperData })
+    }
     if (option.parser) {
       result[key] = option.parser(val, { data, model, option, helperData })
       continue
@@ -105,7 +127,7 @@ export class DataModel {
   /**
    * @param {{[key: string]: Options}} options
    */
-  constructor (options) {
+  constructor(options) {
     this.options = options
   }
 
@@ -117,7 +139,7 @@ export class DataModel {
    * @param {{}} helperData 使用自定义 parser 时的自定义数据
    * @returns {{}}
    */
-  parse (data, helperData) {
+  parse(data, helperData) {
     return parse(data, this.options, helperData)
   }
 }
