9109168a24f33de7703db3c76653b330619ed05d
lllllllqw <465633856@qq.com>
2019-03-12 23:11:07 +0800

    types: add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+declare interface Options {
+  type: ArrayConstructor | Object | Number | String | undefined | null
+  subModel?: {[key: string]: Options}
+  default?: any
+  parser?: (val: any, options: {data: any, model: OptionsMap, option: Options, helperData: any} ) => any
+  fromKey?: string
+}
+
+declare interface OptionsMap {
+  [key: string]: Options
+}
+
+declare interface ObjectAny {
+  [key: string]: any
+}
+
+
+declare module 'js-data-model' {
+  export class DataModel {
+    constructor (options: OptionsMap)
+
+    options: Options
+    
+    parse: (data: ObjectAny, helperData: any) => ObjectAny
+  }
+}
